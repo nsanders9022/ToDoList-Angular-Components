b330d6479b0d2195653230d926ff617860c28da3
Nicole Sanders and Tammy Dang <nicole+tammy@>
2017-03-30 09:26:03 -0700

    add a select option for what to filter by and checkboxes to mark tasks as complete

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -5,9 +5,18 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template: `
-  <ul>
-    <li (click)="editButtonHasBeenClicked(currentTask)"*ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
-  </ul>
+  <select (change)="onChange($event.target.value)">
+     <option value="allTasks">All Tasks</option>
+     <option value="completedTasks">Completed Tasks</option>
+     <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+   </select>
+   <ul>
+     <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
+       <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+       <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+       <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button>
+     </li>
+   </ul>
 
   `
 })
@@ -15,18 +24,19 @@ import { Task } from './task.model';
 export class TaskListComponent {
   @Input() childTaskList: Task[];
   @Output() clickSender = new EventEmitter();
+  filterByCompleteness: string = "incompleteTasks";
 
   editButtonHasBeenClicked(taskToEdit: Task) {
     this.clickSender.emit(taskToEdit);
   }
-
-  isDone(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("this task is not done. Better get to work!")
-    }
-  }
+  // 
+  // isDone(clickedTask: Task) {
+  //   if(clickedTask.done === true) {
+  //     alert("This task is done!");
+  //   } else {
+  //     alert("this task is not done. Better get to work!")
+  //   }
+  // }
 
   priorityColor(currentTask){
     if (currentTask.priority === 3){
@@ -37,5 +47,13 @@ export class TaskListComponent {
       return "bg-info";
     }
   }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+     clickedTask.done = setCompleteness;
+   }
+
+  onChange(optionFromMenu) {
+  this.filterByCompleteness = optionFromMenu;
+}
 
 }
